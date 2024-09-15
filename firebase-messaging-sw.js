819b135c935b9aae8a93397ce79a6a7ada7f1574
Prasiddha Acharya <acharyaprasiddha6@gmail.com>
2024-09-15 15:00:48 +0545

    zdcsfd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,18 +3,18 @@ importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js')
 
 var firebaseConfig = {
     apiKey: "AIzaSyCMfEhAeJE_BQ8eQyK6Mn52hiGf3LeXzcg",
-            authDomain: "prasiddhaacharya-com-np.firebaseapp.com",
-            projectId: "prasiddhaacharya-com-np",
-            storageBucket: "prasiddhaacharya-com-np.appspot.com",
-            messagingSenderId: "912047485186",
-            appId: "1:912047485186:web:23dab6d9bc527d8a84d21b",
-            measurementId: "G-TCFKJ034P3"
+    authDomain: "prasiddhaacharya-com-np.firebaseapp.com",
+    projectId: "prasiddhaacharya-com-np",
+    storageBucket: "prasiddhaacharya-com-np.appspot.com",
+    messagingSenderId: "912047485186",
+    appId: "1:912047485186:web:23dab6d9bc527d8a84d21b",
+    measurementId: "G-TCFKJ034P3"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
+messaging.setBackgroundMessageHandler(function (payload) {
     console.log('Received background message ', payload);
     const notificationOptions = {
         body: payload.notification.body,
